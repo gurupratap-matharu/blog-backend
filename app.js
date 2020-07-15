a4d0b5c135ace2dfa04a7ae3337c8311c84a1b70
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-15 16:39:35 -0300

    beta release of the app

---
+++ app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+
+logger.info('connecting to ', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+module.exports = app
