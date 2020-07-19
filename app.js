94c4c2fc1939d1390671971728721c320b92a86c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-19 14:27:02 -0300

    corrected the order as per verbatim

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
 const express = require('express')
 require('express-async-errors')
 const app = express()
-
 const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
 
 logger.info('connecting to ', config.MONGODB_URI)
