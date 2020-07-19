98d4dcb478f67d6beebb6e12fd658e86c8687845
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-19 11:39:38 -0300

    initial test setup ready

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    console.log('cleared')
+
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+    console.log('Blog models created')
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+    console.log('All promises fulfilled!')
+})
 
 test('blogs are returned as json', () => { })
 test('blogs are returned as json', () => { })
