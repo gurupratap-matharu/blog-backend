c0269c89c10d3fc19880bb03d185d7dfeabd5836
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-19 12:33:50 -0300

    basic tests for retreiveing mock data passing

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,18 +8,34 @@ const Blog = require('../models/blog')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    console.log('cleared')
 
     const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
-    console.log('Blog models created')
+
     const promiseArray = blogObjects.map(blog => blog.save())
     await Promise.all(promiseArray)
-    console.log('All promises fulfilled!')
 })
 
-test('blogs are returned as json', () => { })
-test('blogs are returned as json', () => { })
-test('blogs are returned as json', () => { })
+describe('when there are initially some blogs saved', () => {
+    test('all blogs are returned', async () => {
+
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('a specific blog is within the returned blogs', async () => {
+        const response = await api.get('/api/blogs')
+        const contents = response.body.map(blog => blog.title)
+
+        expect(contents).toContain('My favorite Son')
+        expect(contents).toContain('My handsome Golu')
+    })
+})
 
 afterAll(() => {
     mongoose.connection.close()
