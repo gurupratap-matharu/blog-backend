a02b572b624a09857d8c5acd2090feead811ba7c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-21 16:48:31 -0300

    added get endpoint to return all users

--- controllers/users.js
+++ controllers/users.js
@@ -2,15 +2,20 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
     const body = request.body
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
     if (!(body.username && body.password)) {
         response.status(400).end()
     }
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
 
     const user = new User({
         username: body.username,
