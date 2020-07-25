b94e2f29909bcfd0d6fad84eccc15cf49a5423f8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-25 20:39:31 -0300

    removed unused import

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
-const usersRouter = require('../controllers/users')
 
 const userSchema = new mongoose.Schema({
     username: {
