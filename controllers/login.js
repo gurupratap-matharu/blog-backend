b3b8380423c6460e4286b5108fb0451204535f14
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-07-25 22:04:59 -0300

    added conditional to avoid typeError

--- controllers/login.js
+++ controllers/login.js
@@ -18,14 +18,15 @@ loginRouter.post('/', async (request, response) => {
             error: 'invalid username or password'
         })
     }
-
-    const userForToken = {
-        username: user.username,
-        id: user._id
+    else {
+        const userForToken = {
+            username: user.username,
+            id: user._id
+        }
+        const token = jwt.sign(userForToken, process.env.SECRET)
+        response.status(200)
+            .send({ token, username: user.username, name: user.name })
     }
-    const token = jwt.sign(userForToken, process.env.SECRET)
-    response.status(200)
-        .send({ token, username: user.username, name: user.name })
 })
 
 
